d4f22211382e21ed7d1a95ac5898eb034a7ab1c5
brussell123 <bcr@cs.washington.edu>
2012-09-25 18:23:09 -0700

    added script for downloading and interacting with the database

---
+++ LM3Dinstall.m
@@ -0,0 +1,26 @@
+function LM3Dinstall(destImages, destAnnotations, folder)
+%
+% LM3Dinstall(destImages, destAnnotations)
+%
+% To download a single folder add a third argument:
+% LM3Dinstall(destImages, destAnnotations, folder)
+
+HOMEANNOTATIONS = 'http://labelme.csail.mit.edu/Annotations/users/labelme3d';
+HOMEIMAGES = 'http://labelme.csail.mit.edu/Images/users/labelme3d';
+
+% Build SUN index
+disp('Reading folder list')
+if nargin == 3
+    folder = folderlist(HOMEANNOTATIONS, folder);
+else
+    folder = folderlist(HOMEANNOTATIONS);
+end
+
+% create list of folders to copy, copy and rename folders in XML:
+LMinstall(folder, destImages, destAnnotations, HOMEIMAGES, HOMEANNOTATIONS)
+
+% make sure folder names are coherent
+ADMINrenamefolder(destAnnotations);
+
+
+
---
+++ demo.m
@@ -0,0 +1,43 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Step 1: Change this paths to point to the desired location of the
+% LabelMe3D database
+HOMEDATABASE = './LM3D_database';
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Add required toolboxes to path:
+addpath '~/work/MatlabLibraries/LabelMeToolbox';
+addpath '~/work/MatlabLibraries/LabelMe3dToolbox';
+
+% Define needed paths for the database:
+HOMEIMAGES = fullfile(HOMEDATABASE,'Images');
+HOMEANNOTATIONS = fullfile(HOMEDATABASE,'Annotations');
+CACHE_DIR = fullfile(HOMEDATABASE,'cache'); % Location of any 3D outputs
+
+% Download data:
+LM3Dinstall(HOMEIMAGES,HOMEANNOTATIONS);
+
+% Read data into Matlab structure:
+DB = LMdatabase(HOMEANNOTATIONS);
+
+% Get annotation:
+i = 1;
+annotation = DB(i).annotation;
+img = LMimread(DB,i,HOMEIMAGES);
+
+% Plot 3D annotations:
+
+% Create depth maps:
+
+% Generate textured mesh:
+mesh = getSceneMesh(annotation);
+mesh = subDivideMesh(mesh,2);
+mesh = getTextures(mesh,annotation,img);
+
+% Generate VRML file:
+vrmlfile = strrep(annotation.filename,'.jpg','.wrl');
+vrmlfolder = fullfile(CACHE_DIR,'vrml');
+if ~exist(vrmlfolder,'dir')
+  mkdir(vrmlfolder);
+end
+LM2VRML(annotation,mesh,vrmlfile,vrmlfolder);
+display(sprintf('Produced a VRML file here: %s',fullfile(vrmlfolder,vrmlfile)));
