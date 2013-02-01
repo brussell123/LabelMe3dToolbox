d31cdfda834ec393bfce87dba4040d71c6d80dcf
brussell123 <bcr@cs.washington.edu>
2013-02-01 14:07:04 -0800

    added minDepth and maxDepth parameters and base them on prctile

--- LMplot3Dscene.m
+++ LMplot3Dscene.m
@@ -1,29 +1,42 @@
-function LMplot3Dscene(annotation,img)
+function LMplot3Dscene(annotation,img,varargin)
+% Displays the 3D scene.
+%
+% The following plots 3D polygons:
+%
+% LMplot3Dscene(annotation)
+%
+% The following plots a 3D textured model:
+%
+% LMplot3Dscene(annotation,img)
+%
+% You can specify the min/max depth range:
+%
+% minDepth = 1; maxDepth = 10000;
+% LMplot3Dscene(annotation,img,minDepth,maxDepth)
 
 if nargin > 1
-  PlotTexturedScene(annotation,img);
+  PlotTexturedScene(annotation,img,varargin{:});
   return;
 end
 
 P = getCameraMatrix(annotation,'RH');
+if isempty(P)
+  display('Cannot plot since there is no camera matrix associated with this annotation file.');
+  return;
+end
 [K,R,C] = decomposeP(P);
 
 Ncolors = 10;
 colors = hsv(Ncolors);
-% $$$ figure;
-% $$$ plot(C(1),C(3),'ko');
 plot3(C(1),C(2),C(3),'ko');
 hold on;
-% $$$ plot(0,0,'ko');
 plot3(0,0,0,'ko');
-% $$$ plot([C(1); 0],[C(3); 0],'k');
 plot3([C(1); 0],[C(2); 0],[C(3); 0],'k');
 objStrs = [];
 h = [];
 for i = 1:length(annotation.object)
   if isfield(annotation.object(i).world3d,'polygon3d') && ~isempty(annotation.object(i).world3d.polygon3d)
     [X,Y,Z] = getLMpolygon3D(annotation.object(i).world3d.polygon3d);
-% $$$     hi = plot([X X([2:end 1])]',[Z Z([2:end 1])]','Color',colors(mod(i-1,Ncolors)+1,:));
     hi = plot3([X; X([2:end 1])]',[Y; Y([2:end 1])]',[Z; Z([2:end 1])]','Color',colors(mod(i-1,Ncolors)+1,:));
     h(end+1) = hi(1);
     objStrs{end+1} = annotation.object(i).name;
@@ -36,17 +49,30 @@ zlabel('Z');
 legend(h,objStrs);
 cameratoolbar('setmodeGUI','orbit');
 
-function PlotTexturedScene(annotation,img)
+function PlotTexturedScene(annotation,img,minDepth,maxDepth)
 
+% Get depth maps:
 [Xmap,Ymap,Zmap,Nmap] = getXYZmaps(annotation);
 Xmap = double(Xmap);
 Ymap = double(Ymap);
 Zmap = double(Zmap);
 
+% Get min/max depth to display:
+MAX_DEPTH = 16777215;
+zz = Zmap(Zmap~=MAX_DEPTH);
+if nargin < 4
+  maxDepth = prctile(zz,98);
+% $$$   maxDepth = 10000;
+end
+if nargin < 3
+  minDepth = prctile(zz,2);
+% $$$   minDepth = 1;
+end
+
 % Show warped image:
 % $$$ seg = plotPolyEdgeTypes(annotation3D,img,'valid');
-% $$$ mask = (Zmap>1).*(Zmap<10000).*(mean(seg,3)>0);
-mask = (Zmap>1).*(Zmap<10000);
+% $$$ mask = (Zmap>minDepth).*(Zmap<maxDepth).*(mean(seg,3)>0);
+mask = (Zmap>minDepth).*(Zmap<maxDepth);
 Xmap2 = Xmap;
 Ymap2 = Ymap;
 Zmap2 = Zmap;
