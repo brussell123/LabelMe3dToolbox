4a6176bfbc09bc299ef5b3c2ee269d46be9c98dc
brussell123 <bcr@cs.washington.edu>
2013-02-01 22:09:00 -0800

    fixed NaN when finding horizon line

--- getHorizonLine.m
+++ getHorizonLine.m
@@ -18,7 +18,8 @@ P = getCameraMatrix(annotation);
 if ~isempty(P) && isfield(annotation,'imagesize')
   imageSize = [str2num(annotation.imagesize.nrows) str2num(annotation.imagesize.ncols)];
 
-  lh = cross([Project3D2D([1 0 0 0]',annotation); 1],[Project3D2D([0 0 1 0]',annotation); 1]);
+% $$$   lh = cross([Project3D2D([1 0 0 0]',annotation); 1],[Project3D2D([0 0 1 0]',annotation); 1]);
+  lh = cross(P(:,1),P(:,3));
   x1 = cross(lh,cross([1 1 1],[1 2 1])');
   x2 = cross(lh,cross([imageSize(2) 1 1],[imageSize(2) 2 1])');
   x = [x1(1)/x1(3) x2(1)/x2(3)];
